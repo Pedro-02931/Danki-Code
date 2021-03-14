458daf6d577c71253415f8adfe472c76f3d6d253
Pedro-02931 <80173371+Pedro-02931@users.noreply.github.com>
2021-03-14 16:40:46 -0300

    Create Contador.c
    While simples

---
+++ C/Contador.c
@@ -0,0 +1,14 @@
+//Meu primeiro uso do while
+
+#include <stdio.h>
+
+int main()
+{
+    int con=0;
+    while(10>con){
+        con = con + 1;
+        printf("o número atual é %d \n",con);
+    }
+
+    return 0;
+}
